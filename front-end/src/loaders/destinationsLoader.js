1a6b36c53fed9041bcab304d83508e65acc2e342
P1ayMouse <P1ayMouse@users.noreply.github.com>
2025-02-27 17:18:19 +0000

    deploy: b94c5fb7e51fb28826fc1133525486c5b16350c2

--- front-end/src/loaders/destinationsLoader.js
+++
@@ -1,8 +0,0 @@
-import { store } from '../store';
-import { fetchDestinations } from "../store/thunks/destinationsThunk";
-
-export const destinationsLoader = async () => {
-    const result = await store.dispatch(fetchDestinations());
-
-    return result.payload;
-}
