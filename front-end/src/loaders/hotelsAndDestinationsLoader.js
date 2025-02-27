70e57646e15f5d84f475210c69ab5fe4a565b8d7
P1ayMouse <P1ayMouse@users.noreply.github.com>
2025-02-27 03:38:19 +0000

    deploy: e1568ccb130e92759cdf01307390b5a6e2a689b5

--- front-end/src/loaders/hotelsAndDestinationsLoader.js
+++
@@ -1,9 +0,0 @@
-import { destinationsLoader } from "./destinationsLoader";
-import { hotelsLoader } from "./hotelsLoader";
-
-export async function hotelsAndDestinationsLoader() {
-    const destinations = await destinationsLoader();
-    const hotels = await hotelsLoader();
-
-    return { destinations, hotels };
-}
